939a8125d0f5b9942636bd314e7c9dbdaef518cc
Harsh-Gehlot <81151468+Harsh-Gehlot@users.noreply.github.com>
2023-05-15 03:44:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import reportWebVitals from "./src/reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+//     <App />
+  <h1>Hello</h1>
   </React.StrictMode>,
   document.getElementById("root")
 );

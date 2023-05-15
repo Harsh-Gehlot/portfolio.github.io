0462889d49e006bed68c31a6a388a0b68d6367e5
Harsh-Gehlot <harshgehlot13ime@gmail.com>
2023-05-15 21:43:20 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="#"
+                href="https://instagram.com/13ha.rsh"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
